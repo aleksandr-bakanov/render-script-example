49099e731270ee2c5d516e73f1d7c67b641be70e
Alexander Bakanov <bakanov.aleksandr@gmail.com>
2018-06-05 01:40:12 +0300

    Start OddEven and Bitonic sort implementation

--- app/src/main/java/com/example/bav/renderscriptexample/Solver.kt
+++ app/src/main/java/com/example/bav/renderscriptexample/Solver.kt
@@ -6,34 +6,42 @@ import android.renderscript.Element
 import android.renderscript.RenderScript
 
 class Solver(private val context: Context,
-    private val rs: RenderScript = RenderScript.create(context),
-    private val intersectScript: ScriptC_intersect = ScriptC_intersect(rs)) {
+        private val rs: RenderScript = RenderScript.create(context),
+        private val intersectScript: ScriptC_intersect = ScriptC_intersect(rs)) {
 
-  fun solveViaKotlin(aSet: Set<Int>, bSet: Set<Int>): Set<Int> = aSet.intersect(bSet)
+    fun solveViaKotlin(aSet: Set<Int>, bSet: Set<Int>): Set<Int> = aSet.intersect(bSet)
 
-  fun solveViaRenderScript(aSet: Set<Int>, bSet: Set<Int>): Set<Int> {
-    val aArr = aSet.toIntArray().sortedArray()
-    val bArr = bSet.toIntArray()
-    val rArr = IntArray(bArr.size, {0})
+    fun solveViaRenderScript(aSet: Set<Int>, bSet: Set<Int>): Set<Int> {
+        val aArr = aSet.toIntArray().sortedArray()
+        val bArr = bSet.toIntArray()
+        val rArr = IntArray(bArr.size, {0})
 
-    val aAllocation = Allocation.createSized(rs, Element.I32(rs), aArr.size)
-    val bAllocation = Allocation.createSized(rs, Element.I32(rs), bArr.size)
-    val rAllocation = Allocation.createSized(rs, Element.I32(rs), rArr.size)
+        val aAllocation = Allocation.createSized(rs, Element.I32(rs), aArr.size)
+        val bAllocation = Allocation.createSized(rs, Element.I32(rs), bArr.size)
+        val rAllocation = Allocation.createSized(rs, Element.I32(rs), rArr.size)
 
-    aAllocation.copyFrom(aArr)
-    bAllocation.copyFrom(bArr)
+        aAllocation.copyFrom(aArr)
+        bAllocation.copyFrom(bArr)
 
-    intersectScript.bind_aArray(aAllocation)
-    intersectScript._aSize = aArr.size
+        intersectScript.bind_aArray(aAllocation)
+        intersectScript._aSize = aArr.size
 
-    intersectScript.forEach_execute(bAllocation, rAllocation)
+        intersectScript.forEach_execute(bAllocation, rAllocation)
 
-    rAllocation.copyTo(rArr)
+        rAllocation.copyTo(rArr)
 
-    aAllocation.destroy()
-    bAllocation.destroy()
-    rAllocation.destroy()
+        aAllocation.destroy()
+        bAllocation.destroy()
+        rAllocation.destroy()
 
-    return rArr.filter { it != 0 }.toSet()
-  }
+        return rArr.filter { it != 0 }.toSet()
+    }
+
+    fun batcherOddEvenMergeSort(array: IntArray): IntArray {
+        return IntArray(0)
+    }
+
+    fun bitonicSort(array: IntArray): IntArray {
+        return IntArray(0)
+    }
 }
